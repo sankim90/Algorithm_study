5c67228f206593d112a0d1e02165c22a1bbdd060
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2019-04-10 17:03:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,19 +27,4 @@ int main()
 	//BOJ2178();
 	//DFS();
 
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int N;
-	cin >> N;
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j < N-i; j++) {
-			cout << " ";
-		}
-		for (int k = 0; k < i; k++) {
-			cout << "*";
-		}
-		cout << endl;
-	}
 }
