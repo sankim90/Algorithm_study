986e5efbe989ce511aaefdebfc176ecd6955315e
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2019-04-10 16:50:08 +0900

    Update BOJ15649.cpp

--- BOJ15649.cpp
+++ BOJ15649.cpp
@@ -1,51 +1,4 @@
-/*#include<iostream>
-#include <vector>
-using namespace std;
-
-int N, M;
-//bool visit[10];
-int visit[10];
-
-vector <int> stk;
-int i = 0;
-void print() {
-		for (auto i = stk.begin(); i < stk.end(); i++) cout << *i + 1<< ' ';
-
-		cout << '\n';
-		stk.pop_back();
-		visit[i] = false;
-	
-}
-void func() {
-		
-	stk.push_back(0);
-	
-	while (!stk.empty()) {
-		stk.pop_back();
-		visit[stk.size()] = false;
-		for (i = 0; i < N; i++) {
-			if (visit[i] == 0) {
-				visit[i] = true;
-				stk.push_back(i);
-				
-			}
-			if (stk.size() == M) {
-				print();
-			}
-		}
-
-		
-	}
-}
-
-int main()
-{
-	scanf("%d %d", &N, &M);
-	func();
-	return 0;
-}*/
-
-/*#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -64,7 +17,7 @@ void dfs() {
 		if (!visit[i]) {
 			visit[i] = true; 
 			vec.push_back(i);
-			dfs(); //°£´ÜÈ÷ ¾Ö Á¾·áµÇ¸é ´ÙÀ½ ²¨... returnµÇ¼­ ´ÙÀ½ ÁÙ ½ÇÇàÀÌÁö
+			dfs(); //ê°„ë‹¨íˆ ì•  ì¢…ë£Œë˜ë©´ ë‹¤ìŒ êº¼... returnë˜ì„œ ë‹¤ìŒ ì¤„ ì‹¤í–‰ì´ì§€
 			visit[i] = false; 
 			vec.pop_back();
 		}
@@ -76,90 +29,4 @@ int main(void) {
 	//visit = vector<bool>(N + 1, false);
 
 	dfs();
-}*/
-
-/*#include <iostream>
-#include <algorithm>
-#include <math.h>
-
-using namespace std;
-
-int n, r, c;
-int result;
-
-void recursion(int x, int y, int len) {
-
-	if (x == r && y == c) {
-		cout << result << endl;
-		exit(0);
-	}
-
-	if (len == 1) {
-		result++; return;
-	}
-
-	if (!(x <= r && r<x + len && y <= c && c<y + len)) {
-		result += len*len;
-		return;
-	}
-
-	recursion(x, y, len / 2);
-	recursion(x, y + len / 2, len / 2);
-	recursion(x + len / 2, y, len / 2);
-	recursion(x + len / 2, y + len / 2, len / 2);
-
 }
-
-
-
-int main() {
-
-	cin >> n >> r >> c;
-	recursion(0, 0, pow(2, n));
-	
-	return 0;
-
-}*/
-/*
-#include <stdio.h>
-#include <math.h>
-int N, R, C, cnt = 0;
-
-void func(int n, int r, int c) {
-	if (n == 2) {
-		if (r == R && c == C) {
-			printf("%d\n", cnt++);
-			return;
-		}
-		cnt++;
-
-		if (r == R && c+1 == C) {
-			printf("%d\n", cnt++);
-			return;
-		}
-		cnt++;
-
-		if (r+1 == R && c == C) {
-			printf("%d\n", cnt++);
-			return;
-		}
-		cnt++;
-
-		if (r+1 == R && c+1 == C) {
-			printf("%d\n", cnt++);
-			return;
-		}
-		cnt++;
-		return;
-	}
-
-	func(n / 2, r, c);
-	func(n / 2, r, c+n/2);
-	func(n / 2, r+n/2, c);
-	func(n / 2, r+n/2, c+n/2);
-}
-
-void main() {
-	scanf("%d%d%d", &N, &R, &C);
-	func(pow(2, N), 0, 0);
-}*/
