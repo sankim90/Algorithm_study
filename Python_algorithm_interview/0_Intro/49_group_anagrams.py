327dc75c5923a1950c29391e0048273d24fedcb4
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2021-01-15 14:34:42 +0900

    leetcode 49 group anagrams update

---
+++ Python_algorithm_interview/0_Intro/49_group_anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ana = defaultdict(list)
+
+        for word in strs:
+            ana[''.join(sorted(word))].append(word)
+
+        return ana.values()
+
+if "__main__":
+    sol = Solution()
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    sol.groupAnagrams(strs)
