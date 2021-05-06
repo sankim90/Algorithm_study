dd008b4db68aa12602e6af3827c60a0ba0ad7025
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2021-05-06 18:07:33 +0900

    리트코드 42번 넘어려움..

--- Python_algorithm_interview/0_Intro/42_Trapping_Rain_Water.py
+++ Python_algorithm_interview/0_Intro/42_Trapping_Rain_Water.py
@@ -1,36 +1,50 @@
 from typing import List
 
+# 2 pointer Solution
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         if not height:
+#             return 0
+#
+#         volume = 0
+#         left = 0
+#         right = len(height) - 1
+#         left_max = height[left]
+#         right_max = height[right]
+#
+#         while left < right:
+#             left_max = max(height[left], left_max)
+#             right_max = max(height[right], right_max)
+#             # 최고 높이를 향해 투 포인터 이동
+#             if left_max <= right_max:
+#                 volume += left_max - height[left]
+#                 left += 1
+#             else:
+#                 volume += right_max - height[right]
+#                 right -= 1
+#         return volume
+
 class Solution:
     def trap(self, height: List[int]) -> int:
-        start_stk = []
-        end = 0
-        height_val = 0
-        cnt = 0
-        rst = 0
-        # cnt == len(start_stk)
-        for elem in height:
-            if len(start_stk) == 0: #init
-                if elem > 0:
-                    start_stk.append(elem)
-                    height_val = elem
+        stack = []
+        volume = 0
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
 
-            elif height_val < elem: #오르막
-                cnt = len(start_stk)
-                rst = rst + cnt * height_val
-                tmp_len = len(start_stk)
-                for i in range(tmp_len):
-                    tmp = start_stk.pop()
-                    rst = rst - tmp
-            elif height_val > elem: #내리막
-                height_val = elem
+                if not len(stack):
+                    break
 
-            else:
-                start_stk.append(elem)
+                # 이전과의 차이만큼 물 높이 처리
+                dist = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
 
+                volume += dist * waters
 
+            stack.append(i)
+        return volume
 
 if "__main__":
     sol = Solution()
     input_data = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
     ret = sol.trap(input_data)
-    print(ret)
