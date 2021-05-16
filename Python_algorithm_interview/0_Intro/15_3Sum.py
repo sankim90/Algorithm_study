fa40ebc52bc45662bad18ae405a1933070d0b008
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2021-05-16 23:33:46 +0900

    15번 세수의 합

--- Python_algorithm_interview/0_Intro/15_3Sum.py
+++ Python_algorithm_interview/0_Intro/15_3Sum.py
@@ -1,21 +1,39 @@
 from typing import List
-from itertools import combinations, permutations, combinations_with_replacement
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        tmp = list(combinations(nums, 3))
-        # print(tmp)
-        rst = []
-        for idx, val in enumerate(tmp):
-            tmp2 = sum(val)
-            if tmp2 == 0 and list(val) not in rst:
-                rst.append(list(val))
+        result = []
+        nums.sort()
 
-        return rst
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]: # i = 0일때, i - 1 = -1 즉 index 맨앞과 맨끝 비교
+                continue
+
+            # 간격을 좁혀가며 sum 계산, left right는 index
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    # sum = 0인 경우이므로 정답 및 스킵 처리
+                    result.append((nums[i], nums[left], nums[right]))
+
+                    while left < right and nums[left]  == nums[left  + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+        return result
+
 
 if "__main__":
     sol = Solution()
-    rst = sol.threeSum([-1, 0, 1, 2, -1, -4])
-    # rst = sol.threeSum([0])
+    rst = sol.threeSum([-1,0,1,2,-1,-4])
     print(rst)
