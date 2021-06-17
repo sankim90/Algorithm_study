7069d5682b8fd7695dc355d56551bb4c9697daf3
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2021-06-17 19:03:08 +0900

    121번 DP 풀이 참고

--- Python_algorithm_interview/0_Intro/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ Python_algorithm_interview/0_Intro/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,12 +1,12 @@
 from typing import List
-
+import math
 #https://ip99202.github.io/posts/%EC%B9%B4%EB%8D%B0%EC%9D%B8-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98(DP)/
 #해당 설명 참고, 카데인 알고리즘(DP)
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        min_value = 0
+        min_value = math.inf
 
         for i in prices:
             min_value = min(min_value, i)
