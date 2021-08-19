54c446bcc77e418f54175bc2aedb62a8eea4443a
KIM JINSAN <36911945+sankim90@users.noreply.github.com>
2021-08-19 16:40:11 +0900

    Carry를 생각해 낼 수 있는가? while에 carry를 넣어서 끝처리 해줘야함 (99+99 케이스)

--- Python_algorithm_interview/0_Intro/2_Add_Two_Numbers.py
+++ Python_algorithm_interview/0_Intro/2_Add_Two_Numbers.py
@@ -7,7 +7,28 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        print("HI")
+        carry = 0
+        result_list = tmp_list = ListNode()
+
+        while l1 or l2 or carry:
+            if l1:
+                l1_tmp = l1.val
+                l1 = l1.next
+            if l2:
+                l2_tmp = l2.val
+                l2 = l2.next
+
+            sum = l1_tmp + l2_tmp + carry
+
+            adder = sum % 10
+            carry = sum // 10
+            # print("addr {} carry {}".format(adder, carry))
+
+            l1_tmp, l2_tmp = 0, 0
+            tmp_list.next = ListNode(adder)
+            tmp_list = tmp_list.next
+
+        return result_list.next
 
 
 if "__main__":
@@ -16,12 +37,14 @@ if "__main__":
     link2 = ListNode()
     link3 = ListNode()
     link4 = ListNode()
-    link1.val = 1
+
+    link1.val = 9
     link1.next = link2
-    link2.val = 2
-    link2.next = link3
-    link3.val = 3
+    link2.val = 9
+
+    link3.val = 9
     link3.next = link4
-    link4.val = 4
+    link4.val = 9
+
 
     print(sol.addTwoNumbers(link1, link2))
