d57d7d847232d5c74cbf05086c7d70cb112cd697
KIM JINSAN <jinsankim12@gmail.com>
2023-10-06 12:06:59 +0900

    Update 0x0B.cpp
    그냥 별 보고 그리면 되는데 너무 어렵게 생각했음. 분할 정복인데 이런 문제가 나올까..? draw는 별 그리기, triangle 재귀는 위치 맞춰주기

--- barkking_algorithm/0x0B.cpp
+++ barkking_algorithm/0x0B.cpp
@@ -184,6 +184,35 @@ void boj2447(int n) {
     }
 }
 
+// char arr[3072][6143];
+char arr[30][63];
+void draw(int row, int col)
+{
+    // 첫번째 줄 (꼭짓점에 * 1개)
+    arr[row][col] = '*';
+    
+    // 두번째 줄 (* 2개)
+    arr[row + 1][col - 1] = '*';
+    arr[row + 1][col + 1] = '*';
+    
+    // 세번째 줄 (* 5개)
+    for(int i = 0; i < 5; i++)
+        arr[row + 2][col - 2 + i] = '*';
+}
+
+void triangle(int len, int row, int col)
+{
+    if (len == 3)
+    {
+        draw(row, col);
+        return;  // 리턴의 유무 중요 !!!
+    }
+        
+    triangle(len / 2, row, col);  // 첫 번째 삼각형의 꼭짓점
+    triangle(len / 2, row + len / 2, col - len / 2);  // 두 번째 삼각형의 꼭짓점
+    triangle(len / 2, row + len / 2, col + len / 2);  // 세 번째 삼각형의 꼭짓점
+}
+
 void recur_input() {
     
     int i, j;
@@ -211,7 +240,27 @@ void recur_input() {
 
     // boj2630(N, 0, 0);
     // for (int i = 0; i < 2; i++) cout << cnt2630[i] << "\n";
-    boj2447(N);
+    // boj2447(N);
+     // 별찍기 전에 미리 전체 배열에 공백 넣어주기 
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 2 * N - 1; j++)
+        {
+            arr[i][j] = ' ';
+        }
+    }
+    
+    triangle(N, 0, N-1);  // 별 찍기. 가~장 큰 삼각형부터 넘기기, N-1이 젤 윗 삼각형의 시작을 알림
+    
+    // 출력 
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 2 * N - 1; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << '\n';
+    }
 }
 
 int main()
